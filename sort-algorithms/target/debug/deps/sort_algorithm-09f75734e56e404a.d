aceef3f86edbb15b561aab18dcc315782e0db452
Damien Flury <damien.flury@gmail.com>
2019-09-15 23:57:18 +0200

    Update gitignore

--- sort-algorithms/target/debug/deps/sort_algorithm-09f75734e56e404a.d
+++
@@ -1,5 +0,0 @@
-/home/damienflury/source/sort-algorithm/target/debug/deps/sort_algorithm-09f75734e56e404a: src/main.rs
-
-/home/damienflury/source/sort-algorithm/target/debug/deps/sort_algorithm-09f75734e56e404a.d: src/main.rs
-
-src/main.rs:
--- sort-algorithms/target/debug/sort-algorithm.d
+++
@@ -1,1 +0,0 @@
-/home/damienflury/source/sort-algorithm/target/debug/sort-algorithm: /home/damienflury/source/sort-algorithm/src/main.rs
