fe29f45b5682b9aa2a4e5126293a8a3f6e0722ad
Damien Flury <damien.flury@gmail.com>
2019-09-16 09:32:43 +0200

    Add insertion sort

--- algorithms-and-datastructures/sort-algorithms/src/main.rs
+++ algorithms-and-datastructures/sort-algorithms/src/main.rs
@@ -8,51 +8,59 @@ fn main() {
     println!("Sortieren mit eigenem Algorithmus");
     println!("---------------------------------");
     println!("10 Werte: {:?}", array);
-    let sorted = Algorithms::selection_sort(&mut array);
-    println!("Sortiert: {:?}", sorted);
+    array.insertion_sort();
+    println!("Sortiert: {:?}", array);
 }
 
-struct Algorithms {
+
+trait SortAlgorithms {
+    fn bubble_sort(&mut self) -> &Self;
+    fn selection_sort(&mut self) -> &Self;
+    fn insertion_sort(&mut self) -> &Self;
 }
 
-impl Algorithms {
-    fn sort<T: PartialOrd>(array: &mut [T]) -> &[T] {
-        let length = array.len();
+impl<T: PartialOrd> SortAlgorithms for [T] {
+    fn bubble_sort(&mut self) -> &Self {
+        let length = self.len();
         for i in (0..length - 1).rev() {
             for j in i..length - 1 {
-                if array[j] > array[j + 1] {
-                    array.swap(j, j + 1);
+                if self[j] > self[j + 1] {
+                    self.swap(j, j + 1);
                 }
             }
         }
-        array
+        self
     }
 
-    fn selection_sort<T: PartialOrd + std::fmt::Debug>(array: &mut [T]) -> &[T] {
-        for i in 0..array.len() {
-            let lowest_index = Self::get_lowest_index(&array[i..array.len()]);
-            println!("{}", lowest_index);
-            array.swap(i, i + lowest_index);
+    fn selection_sort(&mut self) -> &Self {
+        for i in 0..self.len() {
+            let lowest_index = get_lowest_index(&self[i..self.len()]);
+            self.swap(i, i + lowest_index);
         }
-        array
+        self
     }
 
-    fn get_lowest_index<T: PartialOrd + std::fmt::Debug>(slice: &[T]) -> usize {
-        println!("{:?}", slice);
-        // slice.iter().skip(1).enumerate().fold((0, &slice[0]), |acc, (index, current)| if current > acc.1 {
-        //     (index, current)
-        // } else {
-        //     acc
-        // }).0
-        let mut min = &slice[0];
-        let mut min_index = 0;
-
-        for (index, item) in slice.iter().enumerate() {
-            if item < min {
-                min = item;
-                min_index = index;
+    fn insertion_sort(&mut self) -> &Self {
+        for i in 0..self.len() {
+            for j in (0..i).rev() {
+                if self[j] >= self[j + 1] {
+                    self.swap(j, j + 1);
+                }
             }
         }
-        min_index
+        self
     }
+}
+
+fn get_lowest_index<T: PartialOrd>(slice: &[T]) -> usize {
+    let mut min = &slice[0];
+    let mut min_index = 0;
+
+    for (index, item) in slice.iter().enumerate() {
+        if item < min {
+            min = item;
+            min_index = index;
+        }
+    }
+    min_index
 }
