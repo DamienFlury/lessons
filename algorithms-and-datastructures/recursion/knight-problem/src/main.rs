383fad8007f7581c18bf5d5002286d18d8812e8e
Damien Flury <damien.flury@gmail.com>
2019-11-28 20:47:37 +0100

    Start implementing knight-problem

--- algorithms-and-datastructures/recursion/knight-problem/src/main.rs
+++ algorithms-and-datastructures/recursion/knight-problem/src/main.rs
@@ -1,8 +1,26 @@
 fn main() {
-    let board = vec![vec![1, 2, 3], vec![1, 2, 3]];
-    knight_problem(board);
+    let mut board = vec![vec![0; 8]; 8];
+    knight_problem(&mut board, 0, 0, 0);
+    println!("{:?}", board);
 }
 
-fn knight_problem(board: Vec<Vec<u32>>) {
-
+fn knight_problem(board: &mut Vec<Vec<u32>>, x: i32, y: i32, level: u32) {
+    if level == 199 {
+        return;
+    }
+    board[y as usize][x as usize] = level;
+    for i in (-2 as i32)..=2 {
+        for j in (-2 as i32)..=2 {
+            if i == j || i == -j || i == 0 || j == 0 {
+                continue;
+            }
+            if y + i >= 0 && 
+                y + i < 8 && 
+                x + j >= 0 && 
+                x + j < 8 && 
+                board[(y + i) as usize][(x + j) as usize] == 0 {
+                knight_problem(board, x + j, y + i, level + 1);
+            }
+        }
+    }
 }
